a87613a65a79aa0354c879bc947f218dd4dcef8b
Nick <Nick_II2000@mail.ru>
2022-06-14 08:17:07 +0600

    6-th commit

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 'use strict';
 
-const box = document.querySelector('.box');
+const box = document.querySelector('.box'),
+    btn = document.querySelector('button');
 
 // const width = box.clientWidth;
 // const height = box.clientHeight;
 
-const width = box.offsetWidth;
-const height = box.offsetHeight;
+// const width = box.offsetWidth;
+// const height = box.offsetHeight;
+
+const width = box.scrollWidth;
+const height = box.scrollHeight;
 
 console.log(width, height);
+
+btn.addEventListener('click', () => {
+    box.style.height = box.scrollHeight + 'px';
+});
