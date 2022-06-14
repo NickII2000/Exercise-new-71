ae45e69e47f2ce15d27b6f903c6a0aacc7e6dda5
Nick <Nick_II2000@mail.ru>
2022-06-14 08:11:06 +0600

    5-th commit

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 
 const box = document.querySelector('.box');
 
-const width = box.clientWidth;
-const height = box.clientHeight;
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+
+const width = box.offsetWidth;
+const height = box.offsetHeight;
 
 console.log(width, height);
